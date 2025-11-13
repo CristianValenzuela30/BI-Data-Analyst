1c74cbc1d649fa6028ac0a4364394ec4eecf1a9c
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-13 10:31:03 +0000

    add drop schema wrapper

---
+++ macros/drop_my_schema.sql
@@ -0,0 +1,4 @@
+{% macro drop_my_schema(schema_name) %}
+  {% set relation = api.Relation.create(database=target.database, schema=schema_name) %}
+  {% do adapter.drop_schema(relation) %}
+{% endmacro %}

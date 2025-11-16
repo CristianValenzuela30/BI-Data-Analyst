1b01151561827f3714d858b0b53c268dd96d61e1
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-16 21:53:30 +0000

    updated model 2

--- models/intermediate/int_impute_bedrooms_wc.sql
+++ models/intermediate/int_impute_bedrooms_wc.sql
@@ -1,9 +1,12 @@
-{{impute_bedrooms_wc(
-    Category,
-    Number_Of_Bedrooms = 'COALESCE(Number_Of_Bedrooms, 0)',
-    Number_Of_WC = 'COALESCE(Number_Of_WC, 0)',
-    Living_Area = 'Living_Area'
-)}}
-SELECT *, Bedrooms_Imputed as Number_Of_Bedrooms_Imputed,
-WC_Imputed as Number_Of_WC_Imputed
-FROM {{ref('stg_raw_property_listings')}}
+{{ impute_bedrooms_wc(
+    category           = 'Category',
+    number_of_bedrooms = 'COALESCE(Number_Of_Bedrooms, 0)',
+    number_of_wc       = 'COALESCE(Number_Of_WC, 0)',
+    living_area        = 'Living_Area'
+) }}
+
+SELECT 
+    *,
+    Bedrooms_Imputed AS Number_Of_Bedrooms_Imputed,
+    WC_Imputed       AS Number_Of_WC_Imputed
+FROM {{ ref('stg_raw_property_listings') }}

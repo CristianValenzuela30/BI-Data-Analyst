aa2c2e0ad4e76bc2841db83fb11ff1b39660f772
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-16 22:52:31 +0000

    --

--- models/intermediate/int_impute_bedrooms_wc.sql
+++ models/intermediate/int_impute_bedrooms_wc.sql
@@ -17,3 +17,6 @@ SELECT
     ) }} as Number_Of_WC_Imputed
 
 FROM {{ ref('stg_raw_property_listings') }}
+-- Add filters to exclude impossible values
+WHERE Number_Of_WC BETWEEN 0 AND 10  -- WC should be between 0-10
+  AND Number_Of_Bedrooms BETWEEN 0 AND 10  -- Bedrooms should be reasonable too

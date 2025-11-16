dd11088c0dc0399c3dec1a3badeeaf832e606a4b
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-16 21:59:00 +0000

    updated 2nd model

--- models/intermediate/int_impute_bedrooms_wc.sql
+++ models/intermediate/int_impute_bedrooms_wc.sql
@@ -1,11 +1,16 @@
-{{ impute_bedrooms_wc(
-    category           = 'Category',
-    number_of_bedrooms = 'COALESCE(Number_Of_Bedrooms, 0)',
-    number_of_wc       = 'COALESCE(Number_Of_WC, 0)',
-    living_area        = 'Living_Area'
-) }}
+{{ config(materialized='view') }}
 
-SELECT 
+-- Call the macro and return the two imputed columns
+{{ impute_bedrooms_wc(
+      Category           = 'Category',
+      Number_Of_Bedrooms = 'COALESCE(Number_Of_Bedrooms, 0)',
+      Number_Of_WC       = 'COALESCE(Number_Of_WC, 0)',
+      Living_Area        = 'Living_Area'
+   )
+}}
+
+-- Select everything from the staging model + the imputed columns
+SELECT
     *,
     Bedrooms_Imputed AS Number_Of_Bedrooms_Imputed,
     WC_Imputed       AS Number_Of_WC_Imputed

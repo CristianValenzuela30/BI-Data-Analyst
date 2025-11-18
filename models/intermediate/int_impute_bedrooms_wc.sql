60209bf62321943843aafd781b4a05bb5cc421b2
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-18 23:17:18 +0000

    updated model int

--- models/intermediate/int_impute_bedrooms_wc.sql
+++ models/intermediate/int_impute_bedrooms_wc.sql
@@ -4,19 +4,16 @@ SELECT
     *,
     {{ impute_bedrooms(
         'Category',
-        'Number_Of_Bedrooms', 
-        'Number_Of_WC',
-        'Living_Area'
-    ) }} as Number_Of_Bedrooms_Imputed,
-    
-    {{ impute_wc(
-        'Category', 
         'Number_Of_Bedrooms',
         'Number_Of_WC',
         'Living_Area'
-    ) }} as Number_Of_WC_Imputed
+    ) }} AS Number_Of_Bedrooms_Imputed,
 
+    {{ impute_wc(
+        'Category',
+        'Number_Of_Bedrooms',
+        'Number_Of_WC',
+        'Living_Area'
+    )}} AS Number_Of_WC_Imputed
+    
 FROM {{ ref('stg_raw_property_listings') }}
--- Add filters to exclude impossible values
-WHERE Number_Of_WC BETWEEN 0 AND 10  -- WC should be between 0-10
-  AND Number_Of_Bedrooms BETWEEN 0 AND 10  -- Bedrooms should be reasonable too

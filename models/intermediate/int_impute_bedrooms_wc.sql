a0c2fcbeeed0d46b77ed034c280425bc7e7dd549
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-19 23:25:45 +0000

    updated logic to deduplicate, added more columns to create SK

--- models/intermediate/int_impute_bedrooms_wc.sql
+++ models/intermediate/int_impute_bedrooms_wc.sql
@@ -21,14 +21,17 @@ SELECT
     'city',
     'town', 
     'district',
-    'category',           
+    'category',
+    'conservation_status',
+    'has_parking',
+    'elevator',
+    'garage',           
     'living_area',        
     'lot_size',           
     'construction_year',  
     'floor',              
     'number_of_bedrooms_imputed',  
-    'number_of_wc_imputed',
-    'publish_date'        
+    'number_of_wc_imputed',     
     ]) }} AS property_surrogate_key
 
 FROM {{ ref('stg_raw_property_listings') }}

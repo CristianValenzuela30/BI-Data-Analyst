28da84642576032a237fbd6c65fb41944c925a98
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-20 22:04:17 +0000

    lk

--- models/marts/real_estate/dimensions/dim_listing_attributes.sql
+++ models/marts/real_estate/dimensions/dim_listing_attributes.sql
@@ -1,16 +1,18 @@
-{{config(materialized='view')}}
+{{ config(materialized='view') }}
 
-select distinct
-    {{dbt_utils.generate_surrogate_key([
-        'energy_certificate',
-        'has_parking',
-        'elevator',
-        'garage',
-        'electric_car_charge'
-    ])}} as attribute_key,
+SELECT DISTINCT
+    {{ dbt_utils.generate_surrogate_key([
+        'COALESCE(energy_certificate, ''Unknown'')',
+        'COALESCE(has_parking::TEXT, ''False'')',
+        'COALESCE(elevator::TEXT, ''False'')',
+        'COALESCE(garage::TEXT, ''False'')',
+        'COALESCE(electric_car_charge::TEXT, ''False'')'
+    ]) }} AS attribute_key,
+
     energy_certificate,
     has_parking,
     elevator,
     garage,
     electric_car_charge
-from {{ref('int_deduplicated_properties')}}
+
+FROM {{ ref('int_deduplicated_properties') }}

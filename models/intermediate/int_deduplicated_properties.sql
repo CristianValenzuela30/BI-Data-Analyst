f60eea433045b30d49c042ce3fa0bcf360103ff2
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-20 15:48:55 +0000

    added deduplicated model

--- models/intermediate/int_deduplicated_properties.sql
+++ models/intermediate/int_deduplicated_properties.sql
@@ -1,19 +1,78 @@
-{{config(materialized='view')}}
+WITH base AS (
+    SELECT
+        *,
+        /* -----------------------------------------------
+           1. Partition by ALL columns except Price
+              (since PublishDate is identical for duplicates)
+        ------------------------------------------------ */
+        ROW_NUMBER() OVER (
+            PARTITION BY
+                district, /*-- 18 coolumns in ttoal selected*/
+                city,
+                town,
+                category,
+                energy_certificate,
+                living_area,
+                lot_size,
+                parking,
+                has_parking,
+                floor,
+                construction_year,
+                garage,
+                elevator,
+                electric_car_charge,
+                raw_number_of_bedrooms,
+                conservation_status,
+                raw_number_of_wc,
+                publish_date
+            ORDER BY publish_date DESC   -- doesn't matter, but safe
+        ) AS rn_by_date,
 
-with base_data as (
-    select * from {{ref('int_impute_bedrooms_wc')}}
+        FIRST_VALUE(price) OVER (
+            PARTITION BY
+                district, /*-- 18 coolumns in ttoal selected*/
+                city,
+                town,
+                category,
+                energy_certificate,
+                living_area,
+                lot_size,
+                parking,
+                has_parking,
+                floor,
+                construction_year,
+                garage,
+                elevator,
+                electric_car_charge,
+                raw_number_of_bedrooms,
+                conservation_status,
+                raw_number_of_wc,
+                publish_date
+            ORDER BY publish_date DESC
+        ) AS base_price
+
+    FROM {{ ref('stg_raw_property_listings') }}
 ),
 
-ranked_properties as (
-    select 
+price_check AS (
+    SELECT
         *,
-        row_number() over(partition by 
-                            property_surrogate_key 
-                            order by 
-                            publish_date DESC, loaded_at DESC) as duplicate_rank
-    from base_data
+        ABS(Price - base_price) / NULLIF(base_price, 0) AS price_variation
+    FROM base
+),
+
+filtered AS (
+    SELECT
+        *
+    FROM price_check
+    WHERE
+        /* Rule A: Price difference ≤ 8% → keep only 1 row */
+        (price_variation <= 0.08 AND rn_by_date = 1)
+
+        OR
+
+        /* Rule B: Price difference > 10% → keep all (treated as unique) */
+        (price_variation > 0.08)
 )
 
-select * exclude (duplicate_rank)
-from ranked_properties
-where duplicate_rank = 1
+SELECT * FROM filtered;

13a5d5a2aabb076e87704638102fd34f2e41624d
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-20 19:05:16 +0000

    added a target folder in warehouse - analytics

--- models/marts/real_estate/dimensions/dim_date.sql
+++ models/marts/real_estate/dimensions/dim_date.sql
@@ -1,3 +1,6 @@
-{{config(materialized='table')}}
+{{config(
+    materialized='table',
+    schema = 'analytics'
+)}}
 
-{{ dbt_date.get_date_dimension('2011-07-05', '2030-12-31')}}
+{{ dbt_date.get_date_dimension('2011-07-05', '2030-12-31')}}a

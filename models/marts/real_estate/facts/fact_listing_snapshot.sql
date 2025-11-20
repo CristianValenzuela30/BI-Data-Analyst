538cd15b35651de631996e8d4decd09762bd1664
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-20 21:55:24 +0000

    ...

--- models/marts/real_estate/facts/fact_listing_snapshot.sql
+++ models/marts/real_estate/facts/fact_listing_snapshot.sql
@@ -1,11 +1,10 @@
 {{ config(materialized='table') }}
 
-SELECT
-    {{ dbt_utils.generate_surrogate_key(['s.property_surrogate_key', 's.publish_date']) }} AS fact_key,
-
+select
+    {{ dbt_utils.generate_surrogate_key(['property_surrogate_key', 's.publish_date']) }} as fact_key,
     p.property_key,
     l.location_key,
-    d.date_day as date_key,
+    d.date_key,
     a.attribute_key,
     s.price,
     s.living_area,
@@ -19,18 +18,19 @@ SELECT
     s.electric_car_charge,
     s.publish_date,
     s.loaded_at
-
-FROM {{ ref('int_deduplicated_properties') }} AS s
-LEFT JOIN {{ ref('dim_property') }} AS p
-    ON s.property_surrogate_key = p.property_surrogate_key
-   AND s.publish_date = p.valid_from                     -- THIS IS THE FIX
-LEFT JOIN {{ ref('dim_location') }} AS l
-    USING (city, town, district)
-LEFT JOIN {{ ref('dim_date') }} AS d
-    ON s.publish_date = d.date_day
-LEFT JOIN {{ ref('dim_listing_attributes') }} AS a
-    ON s.energy_certificate     = a.energy_certificate
-   AND s.has_parking            = a.has_parking
-   AND s.elevator               = a.elevator
-   AND s.garage                 = a.garage
-   AND s.electric_car_charge    = a.electric_car_charge
+from {{ ref('int_deduplicated_properties') }} as s
+left join {{ ref("dim_property") }} as p 
+    on s.property_surrogate_key = p.property_surrogate_key 
+    and s.publish_date = p.valid_from  -- FIX: Use valid_from instead of publish_date
+left join {{ ref('dim_location') }} as l 
+    on s.city = l.city 
+    and s.town = l.town 
+    and s.district = l.district
+left join {{ ref('dim_date') }} as d 
+    on s.publish_date = d.date_day
+left join {{ ref('dim_listing_attributes') }} as a
+    on s.energy_certificate = a.energy_certificate
+    and s.has_parking = a.has_parking
+    and s.elevator = a.elevator
+    and s.garage = a.garage
+    and s.electric_car_charge = a.electric_car_charge

ffa8c5a376c3de0eded6492763bd98b41b53e2fe
Cristian Alfonso Valenzuela Arcos <cristianvalenzuelaarcos@gmail.com>
2025-11-22 15:29:31 +0000

    zzz

--- models/marts/real_estate/optimization/kpi_city_property_summary.sql
+++ models/marts/real_estate/optimization/kpi_city_property_summary.sql
@@ -1,4 +1,3 @@
-
 {{
     config(
         materialized='table',
@@ -6,21 +5,21 @@
 }}
 
 SELECT
-    CITY,
-    TOWN,
-    DISTRICT,
+    l.CITY,
+    l.TOWN,
+    l.DISTRICT,
     COUNT(*) as total_properties,
-    AVG(PRICE) as avg_price,
-    AVG(LIVING_AREA) as avg_living_area,
-    AVG(LOT_SIZE) as avg_lot_size,
-    AVG(BEDROOMS) as avg_bedrooms,
-    AVG(BATHROOMS) as avg_bathrooms,
+    AVG(pf.PRICE) as avg_price,
+    AVG(pf.LIVING_AREA) as avg_living_area,
+    AVG(pf.LOT_SIZE) as avg_lot_size,
+    AVG(pf.BEDROOMS) as avg_bedrooms,
+    AVG(pf.BATHROOMS) as avg_bathrooms,
     -- Percentage calculations
-    SUM(CASE WHEN ELEVATOR = TRUE THEN 1 ELSE 0 END) / COUNT(*) as elevator_pct,
-    SUM(CASE WHEN GARAGE = TRUE THEN 1 ELSE 0 END) as garage_count,
+    SUM(CASE WHEN pf.ELEVATOR = TRUE THEN 1 ELSE 0 END) / COUNT(*) as elevator_pct,
+    SUM(CASE WHEN pf.GARAGE = TRUE THEN 1 ELSE 0 END) as garage_count,
     -- Latest data timestamp for freshness tracking
-    MAX(PUBLISH_DATE) as last_publish_date,
+    MAX(pf.PUBLISH_DATE) as last_publish_date,
     CURRENT_TIMESTAMP() as dbt_updated_at
 FROM {{ ref('fact_listing_snapshot') }} pf
 JOIN {{ ref('dim_location') }} l ON pf.LOCATION_KEY = l.LOCATION_KEY
-GROUP BY CITY, TOWN, DISTRICT
+GROUP BY l.CITY, l.TOWN, l.DISTRICT
